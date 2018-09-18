63b8359689552b01c57133e3ef2e30e6b3be8273
Chris Brownell <42624201+chrisbrownell@users.noreply.github.com>
2018-09-18 01:25:19 -0400

    Add files via upload

---
+++ Lab3-Accelerometer-RGB-IR.ino
@@ -0,0 +1,121 @@
+// Basic demo for accelerometer readings from Adafruit LIS3DH
+// Plus: RGB LED variation based on accelerometer readings
+
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_LIS3DH.h>
+#include <Adafruit_Sensor.h>
+#include <LiquidCrystal.h>
+#include "Adafruit_VCNL4010.h"
+
+// Used for software SPI
+#define LIS3DH_CLK 13
+#define LIS3DH_MISO 12
+#define LIS3DH_MOSI 11
+// Used for hardware & software SPI
+#define LIS3DH_CS 10
+
+// software SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS, LIS3DH_MOSI, LIS3DH_MISO, LIS3DH_CLK);
+// hardware SPI
+//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
+// I2C
+Adafruit_LIS3DH lis = Adafruit_LIS3DH();
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+   #define Serial SerialUSB
+#endif
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+// initialize RGB LED pins
+int redPin = 7;
+int greenPin = 8;
+int bluePin = 9;
+
+// uncomment line below for common anode LED
+#define COMMON_ANODE
+
+// initialize VNCL for IR sensor
+Adafruit_VCNL4010 vcnl;
+
+void setup(void) {
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  Serial.println("VCNL4010 test");
+
+  if (! vcnl.begin()){
+    Serial.println("Sensor not found :(");
+    while (1);
+  }
+  Serial.println("Found VCNL4010");
+  Serial.println("LIS3DH test!");
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);  
+  
+  if (! lis.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+    Serial.println("Couldnt start");
+    while (1);
+  }
+  Serial.println("LIS3DH found!");
+  
+  lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 or 16 G!
+  
+  Serial.print("Range = "); Serial.print(2 << lis.getRange());  
+  Serial.println("G");
+}
+
+void loop() {
+  lis.read();      // get X Y and Z data at once
+  // Then print out the raw data
+  Serial.print("X:  "); Serial.print(lis.x); 
+  Serial.print("  \tY:  "); Serial.print(lis.y); 
+  Serial.print("  \tZ:  "); Serial.print(lis.z); 
+  setColor(map(lis.x,-32768,32767,0,255),map(lis.y,-32768,32767,0,255),map(lis.z,-32768,32767,0,255));
+
+  /* Or....get a new sensor event, normalized */ 
+  sensors_event_t event; 
+  lis.getEvent(&event);
+  
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("X:"); lcd.print(event.acceleration.x);
+  Serial.print(" \tY: "); Serial.print(event.acceleration.y); 
+  lcd.print("  Y:"); lcd.print(event.acceleration.y);
+  Serial.print(" \tZ: "); Serial.print(event.acceleration.z); 
+  Serial.println(" m/s^2 ");
+  lcd.setCursor(0, 1);
+  lcd.print("Z:"); lcd.print(event.acceleration.z);
+  lcd.print("  P:"); lcd.print(vcnl.readProximity());
+  
+  
+  Serial.println();
+
+  // print IR sensor data
+  Serial.print("Ambient: "); Serial.println(vcnl.readAmbient());
+  Serial.print("Proximity: "); Serial.println(vcnl.readProximity());
+ 
+  delay(200); 
+}
+
+void setColor(int red, int green, int blue)
+{
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);  
+}
